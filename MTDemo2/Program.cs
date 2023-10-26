e6710c4e4d83be1e81efb5d0661e8193b3b750d0
sauravganguly2018 <sauravganguly2018@gmail.com>
2023-10-26 10:39:30 +0530

    Version 7

---
+++ MTDemo2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MTDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart ts1 = new ThreadStart(M1);
+            Thread t1 = new Thread(ts1);
+
+            ParameterizedThreadStart pts1 = new ParameterizedThreadStart(M2);
+            Thread t2 = new Thread(M2);
+
+            Task tt1 = new Task(M1);
+            Task tt2 = new Task(() => { M2(100); });
+            Task<int> tt3 = new Task<int>(M3);
+            tt3.Start();
+            //fkdjs
+            //fsdsg
+            //fdjgl
+            int v = tt3.Result;
+            //fjldk
+
+            Task<int> tt4 = new Task<int>(() => { return M4(100); });
+            tt4.Start();    
+            v = tt4.Result;
+        }
+        static void M1() { }
+        static void M2(object i) { }
+
+        static int M3() { return 100; }
+
+        static int M4(int i) { return i; }
+    }
+}
